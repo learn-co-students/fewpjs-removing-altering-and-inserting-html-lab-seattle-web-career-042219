e96b0128a31569eb622ede5f16540b878941eaa4
github username <gregm9999@gmail.comgit statusgit config --global user.email>
2019-06-03 15:36:41 -0700

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 document.querySelector("#main").remove();
-let newHeader = document.createElement("h1");
+var newHeader = document.createElement("h1");
 newHeader.id = "victory";
 newHeader.innerHTML = "YOUR-NAME is the champion";
