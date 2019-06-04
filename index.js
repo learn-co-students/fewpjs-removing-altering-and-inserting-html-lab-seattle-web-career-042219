33930e58530549b5c418bc9b2642d7616ecfd443
austenhasty <hastyae@gmail.com>
2019-06-03 22:37:23 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+main.remove();
+
+// let newHeader = document.body.appendChild('h1#victory');
+let newHeader = document.createElement('H1');
+newHeader.id = "victory";
+newHeader.innerHTML = "${YOUR-NAME} is the champion"
