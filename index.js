45d6a5890466e116839bdf82d82660f8f683f2dd
github username <github email address>
2019-06-03 18:36:29 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+const name = "Jens Vyff"
+
+document.querySelector("main#main").remove();
+
+const newHeader = document.createElement("h1");
+newHeader.className = "victory";
+newHeader.id = "victory";
+newHeader.innerHTML = "${name} is the champion";
